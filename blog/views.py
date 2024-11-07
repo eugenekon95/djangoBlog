e8b2b5efe8c8fe705bc9e3da7c1fd6ca009c086b
Yevhenii Kononenko <evgenijkononenko0@gmail.com>
2024-11-07 10:53:07 +0200

    Add views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from .models import Post
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+
+def index(request):
+    posts = Post.published.all()
+    return render(request,
+                  'blog/post/index.html',
+                  {'posts': posts})
+
+def show(request, id):
+    post = get_object_or_404(Post,
+                             id=id, status=Post.Status.PUBLISHED)
+    return render(request,
+                  'blog/post/show.html',
+                  {'post': post})
